161c8f6db224417280b8a3be0b1b86f71b717b56
Abiola Olamilekan <abiolawalter2015@gmail.com>
2023-04-17 18:28:04 +0100

    Testimonial page completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 
 import "static/dist/styles.css";
 import { Navbar } from "components";
-import { Home, About, Models } from "pages";
+import { Home, About, Models, TestimonialsPage } from "pages";
 
 const App = () => {
     return (
@@ -12,6 +12,7 @@ const App = () => {
                 <Route index path="/" element={<Home />} />
                 <Route path="about" element={<About />} />
                 <Route path="models" element={<Models />} />
+                <Route path="testimonials" element={<TestimonialsPage />} />
             </Routes>
         </>
     );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Home from "./Home";
 import About from "./About";
 import Models from "./Models";
+import TestimonialsPage from "./TestimonialsPage";
 
-export { Home, About, Models };
+export { Home, About, Models, TestimonialsPage };
